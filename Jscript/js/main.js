edb932e58665d7ada9593a6341141f484844527d
Raphael <raphael.souza@unigranrio.br>
2020-06-27 21:02:22 -0300

    aula 1

---
+++ Jscript/js/main.js
@@ -0,0 +1,25 @@
+/*alert("meu primeiro js");
+var nome = "Raphael dos santos";
+var idade = 35;
+alert(nome + " tem " + idade);
+//console.log(nome.toUpperCase());
+//console.log(idade);
+var n1 = 10;
+var n2 = 3;
+alert(n1 + n2);*/
+
+/*var idade = prompt("Qual a sua idade ?");
+if (idade < 18) {
+    alert("Menor de idade");
+} else {
+    alert("Maior de idade");
+};*/
+
+//repetição
+/*var count;
+for (count = 0; count <= 5; count++) {
+    alert(count);
+};*/
+
+var d = new Date();
+alert(d);
